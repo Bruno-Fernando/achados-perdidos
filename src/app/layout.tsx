f805623d116ebc10051787fbe6de5683640d7a55
Bruno Fernando <brunofernando.sv@gmail.com>
2023-08-07 11:49:19 -0300

    Added react query provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/Toaster";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import AuthProvider from "@/context/AuthProvider";
+import Providers from "@/context/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>{children}</AuthProvider>
+        <Providers>{children}</Providers>
         <Toaster />
       </body>
     </html>
