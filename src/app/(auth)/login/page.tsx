ee85aac3621e36f1ce3ce98159ab22889d721b62
Bruno Fernando <brunofernando.sv@gmail.com>
2023-08-07 17:41:41 -0300

    Update Login page styles

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,41 +1,26 @@
-"use client";
-
-import { Button } from "@/components/ui/Button";
-import { useToast } from "@/hooks/use-toast";
-import { Loader2 } from "lucide-react";
-import { signIn } from "next-auth/react";
-import React, { useState } from "react";
+import Image from "next/image";
+import GoogleLogin from "./_components/GoogleLogin";
 
 function SignIn() {
-  const { toast } = useToast();
-  const [loading, setLoading] = useState(false);
-
-  const loginWithGoogle = async () => {
-    setLoading(true);
-
-    try {
-      await signIn("google");
-    } catch (error) {
-      //toast
-      toast({
-        title: "Houve um problema",
-        description:
-          "Aconteceu um problema durante o processo de login, tente novamente mais tarde ou entre em contato",
-        variant: "destructive",
-      });
-    } finally {
-      setLoading(false);
-    }
-  };
-
   return (
-    <div className="min-h-screen flex justify-center items-center gap-10 flex-col">
-      <p>sigin</p>
-      <Button onClick={loginWithGoogle} disabled={loading}>
-        {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-        Google
-      </Button>
-    </div>
+    <main className="dark bg-black">
+      <div className="mx-auto flex min-h-screen w-full flex-col items-center justify-center gap-10 p-5 md:w-1/2">
+        <div className="mx-auto flex flex-col items-center justify-center gap-10 rounded border border-slate-300 p-5">
+          <div className="flex items-center border-b border-white pb-3">
+            <Image src="/logo.svg" alt="Logo" width={80} height={80} />
+            <h2 className="mr-3 text-center text-2xl text-white">
+              Achados e perdidos UFCG
+            </h2>
+          </div>
+
+          <h3 className="text-center text-white">
+            Clique no botão abaixo para realizar o login com o Google
+          </h3>
+
+          <GoogleLogin />
+        </div>
+      </div>
+    </main>
   );
 }
 
