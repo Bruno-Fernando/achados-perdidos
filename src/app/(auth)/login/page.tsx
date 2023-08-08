fc69a4dbe0ec85fb45b3541acbc8a835ad9f9a52
Bruno Fernando <brunofernando.sv@gmail.com>
2023-08-08 17:35:17 -0300

    Added react-hook-form, next-themes, theme provider and update login styles

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,22 +3,20 @@ import GoogleLogin from "./_components/GoogleLogin";
 
 function SignIn() {
   return (
-    <main className="dark bg-black">
-      <div className="mx-auto flex min-h-screen w-full flex-col items-center justify-center gap-10 p-5 md:w-1/2">
-        <div className="mx-auto flex flex-col items-center justify-center gap-10 rounded border border-slate-300 p-5">
-          <div className="flex items-center border-b border-white pb-3">
-            <Image src="/logo.svg" alt="Logo" width={80} height={80} />
-            <h2 className="mr-3 text-center text-2xl text-white">
-              Achados e perdidos UFCG
-            </h2>
-          </div>
-
-          <h3 className="text-center text-white">
-            Clique no botão abaixo para realizar o login com o Google
-          </h3>
-
-          <GoogleLogin />
+    <main className="flex min-h-screen items-center justify-center p-5">
+      <div className="mx-auto flex flex-col items-center justify-center gap-10 rounded border border-slate-300 p-5">
+        <div className="flex items-center border-b border-white pb-3">
+          <Image src="/logo.svg" alt="Logo" width={80} height={80} />
+          <h2 className="mr-3 text-center text-2xl text-white">
+            Achados e perdidos UFCG
+          </h2>
         </div>
+
+        <h3 className="text-center text-white">
+          Clique no botão abaixo para realizar o login com o Google
+        </h3>
+
+        <GoogleLogin />
       </div>
     </main>
   );
--- src/context/Providers.tsx
+++ src/context/Providers.tsx
@@ -2,13 +2,16 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 const queryClient = new QueryClient();
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <SessionProvider>{children}</SessionProvider>
+      <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem>
+        <SessionProvider>{children}</SessionProvider>
+      </NextThemesProvider>
     </QueryClientProvider>
   );
 }
