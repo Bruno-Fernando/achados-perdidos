1a061e719d2023cb2d592bde08d4f63408d6d39d
Bruno Fernando <brunofernando.sv@gmail.com>
2023-08-09 11:29:45 -0300

    Open browser

--- src/app/api/validate/route.ts
+++ src/app/api/validate/route.ts
@@ -27,66 +27,68 @@ export async function POST(req: Request) {
     // Inicia uma nova instancia do navegador
     const browser = await puppeteer.launch({ headless: "new" });
 
+    return new Response("conseguiu abrir o browser");
+
     // Abre uma aba
-    const page = await browser.newPage();
+    // const page = await browser.newPage();
 
-    // Vai para o controle academico
-    await page.goto(url);
+    // // Vai para o controle academico
+    // await page.goto(url);
 
-    // Digita a matricula e a senha nos campos adequados
-    await page.type("#login", registrationCode);
-    await page.type("#senha", password);
+    // // Digita a matricula e a senha nos campos adequados
+    // await page.type("#login", registrationCode);
+    // await page.type("#senha", password);
 
-    // Submete o form contendo matricula e senha e espera o retorno da api
-    await Promise.all([
-      page.click("button[type=submit]"),
-      page.waitForNavigation({ waitUntil: "networkidle2" }),
-    ]);
+    // // Submete o form contendo matricula e senha e espera o retorno da api
+    // await Promise.all([
+    //   page.click("button[type=submit]"),
+    //   page.waitForNavigation({ waitUntil: "networkidle2" }),
+    // ]);
 
-    // Caso o login tenha acontecido com sucesso, procura na home do
-    // controle academico o número de matricula, caso esteja igual
-    // significa que o login aconteceu com sucesso
-    // caso contrario o login não foi efetuado
-    const isAuth = await page.evaluate((reqRegistrationCode: string) => {
-      const scrapedRegistrationCode = document
-        .querySelector(".col-sm-9 , .col-xs-7")
-        ?.innerHTML.split(" ")[0];
+    // // Caso o login tenha acontecido com sucesso, procura na home do
+    // // controle academico o número de matricula, caso esteja igual
+    // // significa que o login aconteceu com sucesso
+    // // caso contrario o login não foi efetuado
+    // const isAuth = await page.evaluate((reqRegistrationCode: string) => {
+    //   const scrapedRegistrationCode = document
+    //     .querySelector(".col-sm-9 , .col-xs-7")
+    //     ?.innerHTML.split(" ")[0];
 
-      if (scrapedRegistrationCode === reqRegistrationCode) {
-        return true;
-      } else {
-        return false;
-      }
-    }, registrationCode);
+    //   if (scrapedRegistrationCode === reqRegistrationCode) {
+    //     return true;
+    //   } else {
+    //     return false;
+    //   }
+    // }, registrationCode);
 
-    // Fecha a instancia do navegador
-    await browser.close();
+    // // Fecha a instancia do navegador
+    // await browser.close();
 
-    // Atualiza a entidade do usuário caso tenha conseguido
-    // autenticar com o controle academico
-    if (isAuth && session?.user.email) {
-      await db.user.update({
-        where: {
-          email: session.user.email,
-        },
-        data: {
-          ufcgLoginValidated: true,
-        },
-      });
+    // // Atualiza a entidade do usuário caso tenha conseguido
+    // // autenticar com o controle academico
+    // if (isAuth && session?.user.email) {
+    //   await db.user.update({
+    //     where: {
+    //       email: session.user.email,
+    //     },
+    //     data: {
+    //       ufcgLoginValidated: true,
+    //     },
+    //   });
 
-      return NextResponse.json({ isAuth }, { status: 200 });
-    }
+    //   return NextResponse.json({ isAuth }, { status: 200 });
+    // }
 
-    return NextResponse.json(
-      {
-        isAuth: false,
-        errorMessage:
-          "Não foi possível autenticar o usuário, verifique o número de matricula e senha",
-      },
-      {
-        status: 422,
-      },
-    );
+    // return NextResponse.json(
+    //   {
+    //     isAuth: false,
+    //     errorMessage:
+    //       "Não foi possível autenticar o usuário, verifique o número de matricula e senha",
+    //   },
+    //   {
+    //     status: 422,
+    //   },
+    // );
   } catch (error) {
     if (error instanceof z.ZodError) {
       return new Response(error.message, { status: 422 });
