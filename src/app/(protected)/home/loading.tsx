33c9e15796686fdce0ad337bdd5330d7d6434cbc
Bruno Fernando <brunofernando.sv@gmail.com>
2023-12-26 15:47:20 -0300

    Updated home to server component and add loading

---
+++ src/app/(protected)/home/loading.tsx
@@ -0,0 +1,23 @@
+import { Skeleton } from "@/components/ui/Skeleton";
+
+function loading() {
+  return (
+    <div className="flex flex-col gap-4">
+      {Array.apply(null, Array(3)).map(() => (
+        <div className="rounded-lg border border-l-8 border-l-slate-600">
+          <Skeleton className="h-[300px] w-full rounded-lg" />
+
+          <Skeleton className="ml-auto mr-3 mt-1 h-4 w-24" />
+
+          <div className="space-y-2 px-3 pb-3">
+            <Skeleton className="h-5 w-56" />
+
+            <Skeleton className="h-4 w-72" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default loading;
--- src/app/(protected)/home/page.tsx
+++ src/app/(protected)/home/page.tsx
@@ -1,7 +1,22 @@
-import Feed from "../_components/Feed";
+import PostCard from "./_components/PostCard";
+import { getPosts } from "@/actions/posts";
 
-function page() {
-  return <Feed />;
+async function page() {
+  const data = await getPosts();
+
+  return (
+    <div className="flex flex-col gap-4">
+      {data?.map(({ title, description, id, type, createdAt }) => (
+        <PostCard
+          key={id}
+          title={title}
+          description={description}
+          lost={type === "LOST"}
+          createdAt={createdAt || ""}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default page;
