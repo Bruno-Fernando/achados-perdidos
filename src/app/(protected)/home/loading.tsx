3fc22d091ff0c731e1809a692e759095a00fe574
Bruno Fernando <brunofernando.sv@gmail.com>
2023-12-29 09:42:44 -0300

    Add key prop to home loading skeleton

--- src/app/(protected)/home/loading.tsx
+++ src/app/(protected)/home/loading.tsx
@@ -3,8 +3,11 @@ import { Skeleton } from "@/components/ui/Skeleton";
 function loading() {
   return (
     <div className="flex flex-col gap-4">
-      {Array.apply(null, Array(3)).map(() => (
-        <div className="rounded-lg border border-l-8 border-l-slate-600">
+      {Array.apply(null, Array(3)).map((_, index) => (
+        <div
+          className="rounded-lg border border-l-8 border-l-slate-600"
+          key={index}
+        >
           <Skeleton className="h-[300px] w-full rounded-lg" />
 
           <Skeleton className="ml-auto mr-3 mt-1 h-4 w-24" />
