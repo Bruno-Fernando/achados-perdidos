c532097e4a567b70f735a2789c7593232331b472
Bruno Fernando <brunofernando.sv@gmail.com>
2024-01-03 13:53:38 -0300

    Add contact and social share

--- src/app/(protected)/post/[postId]/page.tsx
+++ src/app/(protected)/post/[postId]/page.tsx
@@ -1,19 +1,29 @@
 import { getPost } from "@/actions/posts";
+import { Icons } from "@/components/ExternalIcons";
+import { buttonVariants } from "@/components/ui/Button";
 import { Separator } from "@/components/ui/Separator";
+import { cn } from "@/lib/utils";
+import { Facebook } from "lucide-react";
 import Image from "next/image";
+import CopyToClipboard from "./_components/CopyToClipboard";
 
 async function Post({ params }: { params: { postId: string } }) {
   const data = await getPost(params.postId);
 
   return (
     <div>
-      <p className="mb-4 text-xl">
+      <p className="text-xl">
         <span
           className={data?.type === "FOUND" ? "text-green-600" : "text-red-600"}
         >
           {data?.type === "FOUND" ? "Encontrado: " : "Perdido: "}
         </span>
         {data?.title}
+      </p>
+
+      <p className="mb-4 text-sm">
+        Publicado em {data?.createdAt.toLocaleDateString()} às{" "}
+        {data?.createdAt.toLocaleTimeString()} pelo usuário: {data?.author.name}
       </p>
 
       {data?.imgUrl && (
@@ -34,10 +44,34 @@ async function Post({ params }: { params: { postId: string } }) {
 
       <Separator className="my-8" />
 
-      <p className="text-sm">
-        Publicado em {data?.createdAt.toLocaleDateString()} às{" "}
-        {data?.createdAt.toLocaleTimeString()} pelo usuário: {data?.author.name}
+      <p>
+        Entre em contato através do e-mail:{" "}
+        <a href={`mailto:${data.author.email}`} className="underline">
+          {data.author.email}
+        </a>
       </p>
+
+      <p className="mb-4">Compartilhar:</p>
+
+      <div className="flex gap-2">
+        <a
+          href="https://www.facebook.com/sharer/sharer.php?u=http%3A//localhost%3A3000/post/clqr09fbz00015w787ifkbizd"
+          target="_blank"
+          className={cn(buttonVariants({ variant: "outline" }))}
+        >
+          <Facebook />
+        </a>
+
+        <a
+          href="https://twitter.com/intent/tweet?text=http%3A//localhost%3A3000/post/clqr09fbz00015w787ifkbizd"
+          target="_blank"
+          className={cn(buttonVariants({ variant: "outline" }))}
+        >
+          <Icons.x className="h-6 w-6 fill-current" />
+        </a>
+
+        <CopyToClipboard />
+      </div>
     </div>
   );
 }
