7daa68771343cd775ca73bfd8694a6fa9c419998
Bruno Fernando <brunofernando.sv@gmail.com>
2024-01-06 14:30:48 -0300

    Handle user without posts

--- src/app/(protected)/profile/page.tsx
+++ src/app/(protected)/profile/page.tsx
@@ -12,24 +12,32 @@ async function Profile() {
 
       <Separator className="my-4" />
 
-      <h3 className="text-xl font-bold">Minhas postagens:</h3>
+      {userPosts.length ? (
+        <>
+          <h3 className="text-xl font-bold">Minhas postagens:</h3>
 
-      <div className="mt-4 flex flex-col gap-4">
-        {userPosts.map(
-          ({ id, title, description, type, createdAt, imgUrl }) => (
-            <PostCard
-              key={id}
-              id={id}
-              title={title}
-              description={description}
-              lost={type === "LOST"}
-              createdAt={createdAt || ""}
-              imgUrl={imgUrl}
-              enableEditDelete
-            />
-          ),
-        )}
-      </div>
+          <div className="mt-4 flex flex-col gap-4">
+            {userPosts.map(
+              ({ id, title, description, type, createdAt, imgUrl }) => (
+                <PostCard
+                  key={id}
+                  id={id}
+                  title={title}
+                  description={description}
+                  lost={type === "LOST"}
+                  createdAt={createdAt || ""}
+                  imgUrl={imgUrl}
+                  enableEditDelete
+                />
+              ),
+            )}
+          </div>
+        </>
+      ) : (
+        <h3 className="pt-10 text-center text-xl font-bold">
+          Você não possui postagens ainda
+        </h3>
+      )}
     </>
   );
 }
