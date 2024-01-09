90615950718664a7e36ea70ce2279b9fb61bd5dc
Bruno Fernando <brunofernando.sv@gmail.com>
2024-01-09 14:59:21 -0300

    Update home loading

--- src/app/(protected)/home/loading.tsx
+++ src/app/(protected)/home/loading.tsx
@@ -1,7 +1,13 @@
 import LoadingPostCard from "@/components/LoadingPostCard";
+import { Skeleton } from "@/components/ui/Skeleton";
 
 function loading() {
-  return <LoadingPostCard qtyCards={4} />;
+  return (
+    <>
+      <Skeleton id="total-items" className="mb-4 h-4 w-32" />
+      <LoadingPostCard qtyCards={4} />;
+    </>
+  );
 }
 
 export default loading;
